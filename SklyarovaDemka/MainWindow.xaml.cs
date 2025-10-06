f94d5986cf777ef3ee915720efd7c855e5c93a63
Skliarova.D.A <Skliarova.D.A@PC205WIN-08>
2025-10-06 14:57:09 +0300

    Add project files.

---
+++ SklyarovaDemka/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SklyarovaDemka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, введите логин и пароль");
+                return;
+            }
+
+            using (var context = new DashaEntities())
+            {
+                var user = await context.Users
+                    .Where(u => u.username == username)
+                    .FirstOrDefaultAsync();
+                if (user != null)
+                {
+                    MessageBox.Show("неправильный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (user.password == password)
+                {
+                    user.LastLoginDate = DateTime.Now;
+                    user.IsFirstLogin = false;
+                    await context.SaveChangesAsync();
+                    MessageBox.Show("Успешная авторизация", "Ураааа", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+        }
+    }
+}
